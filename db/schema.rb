aad91f283d8570ccfc5cb10396a2361aca2c0fb9
carloscodify <carlos.gutierrez@codify.com.bo>
2023-08-16 17:58:29 -0400

    Add devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_193628) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_16_215638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -120,6 +120,31 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_14_193628) do
     t.index ["receiver_customer_id"], name: "index_shipments_on_receiver_customer_id"
     t.index ["sender_customer_id"], name: "index_shipments_on_sender_customer_id"
     t.index ["shipment_status_id"], name: "index_shipments_on_shipment_status_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "customers", "people"
